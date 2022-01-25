6717059b89b42a934eca3e7563fe6778c7870343
Henrik Løvhaug <henrik.lovhaug@gmail.com>
2022-01-25 16:10:12 +0100

    web connect

--- Server/server.py
+++ Server/server.py
@@ -2,8 +2,30 @@ from http.server import HTTPServer, BaseHTTPRequestHandler
 import PyATEMMax
 import time
 import netifaces as ni
+import requests
 
 atem = PyATEMMax.ATEMMax()
+
+prev_preview = 0
+prev_current = 0
+
+
+# State:
+#   0 = Off
+#   1 = Preview
+#   2 = Active
+class Camera():
+    def __init__(self,ip,number):
+        self.ip = ip
+        self.number = number
+        self.state = 0
+
+
+camera1 = Camera("192.168.1.4", 1)
+camera2 = Camera("192.168.1.5", 2)
+camera3 = Camera("192.168.1.6", 3)
+camera4 = Camera("192.168.1.7", 4)
+cameralist: [] = [camera1, camera2, camera3, camera4]
 
 
 def find_atem_ip() -> str:
@@ -21,25 +43,64 @@ def find_atem_ip() -> str:
             break
     return ip
 
-status = {
-    "/cam1": "live",
-    "/cam2": "inactive",
-    "/cam3": "inactive",
-    "/cam4": "inactive",
-}
 
 def is_atem_ip(ip) -> bool:
     atem.ping(ip)
     return atem.waitForConnection(waitForFullHandshake=False)
 
-def on_camera_change(params) -> (int,int):
-    print("--------------------------")
-    print("Current input: ",atem.programInput[0].videoSource)
-    print("Current preview: ",atem.previewInput[0].videoSource)
-    print("--------------------------")
-    current = list(str(atem.programInput[0].videoSource))[5] if list(str(atem.programInput[0].videoSource))[0] == "i" else 0
-    preview = list(str(atem.previewInput[0].videoSource))[5] if list(str(atem.previewInput[0].videoSource))[0] == "i" else 0
-    print(current)
+
+def on_camera_change(params):
+    global prev_preview
+    global prev_current
+
+    # print("--------------------------")
+    # print("Current input: ", atem.programInput[0].videoSource)
+    # print("Current preview: ", atem.previewInput[0].videoSource)
+    # print("--------------------------")
+    current = list(str(atem.programInput[0].videoSource))[5] if list(
+        str(atem.programInput[0].videoSource))[0] == "i" else 0
+
+    preview = list(str(atem.previewInput[0].videoSource))[5] if list(
+        str(atem.previewInput[0].videoSource))[0] == "i" else 0
+
+    if prev_current != current:
+        update_camera(cameralist[int(current)-1 if current != 0 else 0], 2)
+        update_camera(cameralist[int(prev_current)-1 if current != 0 else 0], 0)
+        prev_current = current
+
+    if prev_preview != preview:
+        update_camera(cameralist[int(prev_preview)-1 if current != 0 else 0], 0)
+        if preview != current:
+            update_camera(cameralist[int(preview)-1 if current != 0 else 0], 1)
+        prev_preview = preview
+
+    #print(current)
+
+
+def update_camera(camera: Camera, state):
+    if state == 2:
+        try:
+            r = requests.get("http://"+camera.ip)
+            print(r.status_code)
+            print("Send turn on request")
+        except OSError:
+            print("failed to connect")
+            pass
+    elif state == 1:
+        try:
+            r = requests.get("http://"+camera.ip+"/preview")
+            print(r.status_code)
+            print("Sent preview request")
+        except OSError:
+            pass
+    else:
+        try:
+            r = requests.get("http://"+camera.ip+"/off")
+            print(r.status_code)
+            print("Sent preview request")
+        except OSError:
+            pass
+
 
 
 class Handler(BaseHTTPRequestHandler):
@@ -62,7 +123,8 @@ class Handler(BaseHTTPRequestHandler):
 if __name__ == "__main__":
     # ip = find_atem_ip()
     ip = "192.168.0.240"
-    atem = PyATEMMax.ATEMMax() # When disconnected, variable should be reassigned. Why? I dunno, it works
+    # When disconnected, variable should be reassigned. Why? I dunno, it works
+    atem = PyATEMMax.ATEMMax()
     atem.registerEvent(atem.atem.events.receive, on_camera_change)
     atem.connect(ip)
     atem.waitForConnection()
@@ -71,4 +133,3 @@ if __name__ == "__main__":
         # print(atem.connected)
         # print(ip)
         time.sleep(0.5)
-
