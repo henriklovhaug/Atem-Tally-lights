eb5b9a070edbb582fc4b635a8101e31711a8f1b6
Henrik <henrik@stud.ntnu.no>
2022-02-06 18:48:48 +0100

    LED strip support for esp

--- ESP32/include/config.hpp
+++ ESP32/include/config.hpp
@@ -14,8 +14,8 @@ struct ESPconfig
     const char *SSID = "ESP32";
     const char *PASSWORD = "12345678";
     int CameraNumber = 4;
-    IPAddress local_ip = IPAddress(192, 168, 1, 4); //Change this to your local IP
-    IPAddress gateway = IPAddress(192, 168, 1, 1);
+    IPAddress local_ip = IPAddress(192, 168, 2, 4); //Change this to your local IP
+    IPAddress gateway = IPAddress(192, 168, 2, 1);
     IPAddress subnet = IPAddress(255, 255, 255, 0);
 };
 
--- ESP32/src/main.cpp
+++ ESP32/src/main.cpp
@@ -1,15 +1,22 @@
 #include <wificonfig.hpp>
 #include <WebServer.h>
+#include <Adafruit_NeoPixel.h>
 
 
-uint8_t LEDRed = GPIO_NUM_34;
+uint8_t LEDRed = GPIO_NUM_23;
 bool LED1status = LOW;
 
-uint8_t LEDGreen = GPIO_NUM_35;
+uint8_t LEDGreen = GPIO_NUM_22;
 bool LED2status = LOW;
 
-uint8_t LEDBlue = GPIO_NUM_26;
+uint8_t LEDBlue = GPIO_NUM_21;
 bool LED3status = LOW;
+
+#define LEDSTRIP_PIN GPIO_NUM_18
+uint8_t LEDSTRIP_COUNT = 3;
+bool LEDSTRIP_STATUS = true;
+
+Adafruit_NeoPixel pixels(LEDSTRIP_COUNT, LEDSTRIP_PIN, NEO_GRB + NEO_KHZ800);
 
 WebServer server(80);
 
@@ -24,12 +31,21 @@ void handle_live_on()
 {
     server.send(200);
     digitalWrite(LEDRed, HIGH);
+    if(LEDSTRIP_STATUS)
+    {
+        for(int i=0; i<LEDSTRIP_COUNT; i++)
+        {
+            pixels.setPixelColor(i, pixels.Color(255, 0, 0));
+            pixels.show();
+        }
+    }
     Serial.println("Got request for live on");
 }
 
 void handle_live_off()
 {
     server.send(200);
+    pixels.clear();
     digitalWrite(LEDRed, LOW);
     Serial.println("Got request for live off");
 }
@@ -48,24 +64,37 @@ void handle_preview_off()
     Serial.println("Got request for preview off");
 }
 
+void handle_led_strip_on(){
+    server.send(200);
+    LEDSTRIP_STATUS = true;
+}
+
+void handle_led_strip_off(){
+    server.send(200);
+    LEDSTRIP_STATUS = false;
+}
+
 void setup()
 {
     Serial.begin(9600);
 
-    ConnectToWiFi();
+    pinMode(LEDRed, OUTPUT);
+    pinMode(LEDGreen, OUTPUT);
+    pinMode(LEDBlue, OUTPUT);
+    // ConnectToWiFi();
     server.on("/live/1", handle_live_on);
     server.on("/live/0", handle_live_off);
     server.on("/preview/1", handle_preview_on);
     server.on("/preview/0", handle_preview_off);
+    server.on("/led/1", handle_led_strip_on);
+    server.on("/led/0", handle_led_strip_off);
     server.onNotFound([]() {
         server.send(200, "text/html", HTML);
     });
     server.begin();
     delay(100);
+    pixels.begin();
 
-    pinMode(LEDRed, OUTPUT);
-    pinMode(LEDGreen, OUTPUT);
-    pinMode(LEDBlue, OUTPUT);
 }
 
 /* Main loop */
@@ -82,4 +111,5 @@ void loop()
     {
         digitalWrite(LEDBlue, LOW);
     }
+
 }
