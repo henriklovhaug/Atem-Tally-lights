232549054769d7e471c43ba7f9f5d14339854ab9
tobiashaugeland <tobias.haugeland@gmail.com>
2022-02-10 20:32:24 +0100

    find all sources within current scene

--- OBS-script/getscene.py
+++ OBS-script/getscene.py
@@ -21,9 +21,17 @@ def script_update(settings):
 
 def script_tick(seconds):
     global last_scene, logging_enabled
-    current_scene = obs.obs_frontend_get_current_scene()
-    name = obs.obs_source_get_name(current_scene)
+    current_scene_as_source = obs.obs_frontend_get_current_scene()
+    current_scene = obs.obs_scene_from_source(current_scene_as_source)
+    name = obs.obs_source_get_name(current_scene_as_source)
+    
     if name != last_scene:
         last_scene = name
         if logging_enabled:
-            print(last_scene)
+            # print(obs.obs_scene_enum_items(current_scene))
+            print(f"Scene: {last_scene}")
+            print("Sources: ")
+            for scene_item in obs.obs_scene_enum_items(current_scene):
+                as_source = obs.obs_sceneitem_get_source(scene_item)
+                print(obs.obs_source_get_name(as_source))
+            print()
