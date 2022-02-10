c9fb85a98f9aaeb2799bdb3eb262867a9ab156b8
tobiashaugeland <tobias.haugeland@gmail.com>
2022-02-10 18:30:22 +0100

    added checkbox

--- OBS-script/getscene.py
+++ OBS-script/getscene.py
@@ -1,18 +1,29 @@
 import obspython as obs
-from time import sleep
+
+last_scene = ""
+logging_enabled = False
 
 
 def script_description():
     return "Script to report active scene"
 
 
-last_scene = ""
+def script_properties():
+    props = obs.obs_properties_create()
+    obs.obs_properties_add_bool(props, "logging_enabled", "Logging")
+    return props
+
+
+def script_update(settings):
+    global logging_enabled
+    logging_enabled = obs.obs_data_get_bool(settings, "logging_enabled")
 
 
 def script_tick(seconds):
-    global last_scene
+    global last_scene, logging_enabled
     current_scene = obs.obs_frontend_get_current_scene()
     name = obs.obs_source_get_name(current_scene)
     if name != last_scene:
         last_scene = name
-        print(last_scene)
+        if logging_enabled:
+            print(last_scene)
